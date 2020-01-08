302fe12f3b6499cbba499e111385801c76380b06
Chun Yan Sit <the-gates-of-Zion@outlook.com>
2020-01-08 14:59:39 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+  ReactDOM.render(
+    <div>This is render from react Dom</div>
+    ,
+    document.getElementById("reactapp"),
+);
